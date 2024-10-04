869a197ea2459eff7517d62ce458ab5c80ce3d95
PerryEx64 <brianisrael74@gmail.com>
2024-10-03 20:13:44 -0600

    migraciones y roles y permisos

---
+++ database/migrations/2024_10_04_020919_beneficiaries.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('sucursales', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->integer('phone')->nullable();
+            $table->unsignedInteger('relationship_id');
+            $table->foreign('relationship_id')->references('id')->on('relationships');
+            $table->unsignedBigInteger('country_id');
+            $table->foreign('country_id')->references('country_id')->on('countrys');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('sucursales');
+    }
+};
