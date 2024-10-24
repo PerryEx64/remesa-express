7abb1e8d77c52bd5f893e01a589da73f265cb50b
BeltetonJosue96 <josuebelteton@icloud.com>
2024-10-24 11:02:58 -0600

    Se implementan vistas para el Método de Pago y Pago con Tarjeta.

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,13 @@
 <?php
 
+use App\Http\Controllers\InfoEnvioController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/metodopago', [InfoEnvioController::class, 'metodopago'])->name('metodopago');
+Route::get('/pago', [InfoEnvioController::class, 'pago'])->name('pago');
 
 Route::middleware([
     'auth:sanctum',
